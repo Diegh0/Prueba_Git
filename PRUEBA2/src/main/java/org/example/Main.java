0b60cd413def3b56f6e4b9b6e49fef34f210c619
Diego Fuertes <115450079+Diegh0@users.noreply.github.com>
2022-11-27 18:04:03 +0100

    Add files via upload

---
+++ PRUEBA2/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int prueba=10
+                if(prueba==10){
+                    System.out.println("helloooooo");
+                }
+    }
+}
